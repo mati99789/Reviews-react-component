c68304aab83a3695a91aef84fbb2e73655cfc669
Mateusz Urbaniak <matteus.urbaniak@hotmail.com>
2021-11-27 16:53:21 +0100

    Updates

---
+++ precache-manifest.2af6df0344f4a5b11cae0f45b99a6341.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "036f13af7fd6a2e46a2b149bd93448b3",
+    "url": "/http//mati99789.github.io/Reviews-react-component/index.html"
+  },
+  {
+    "revision": "d962da534533a3ef5c54",
+    "url": "/http//mati99789.github.io/Reviews-react-component/static/css/main.3d9f8e8d.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/http//mati99789.github.io/Reviews-react-component/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/http//mati99789.github.io/Reviews-react-component/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d962da534533a3ef5c54",
+    "url": "/http//mati99789.github.io/Reviews-react-component/static/js/main.7e801fc7.chunk.js"
+  },
+  {
+    "revision": "5a72bd0f99af1bff715e",
+    "url": "/http//mati99789.github.io/Reviews-react-component/static/js/runtime-main.6e2f805b.js"
+  }
+]);
